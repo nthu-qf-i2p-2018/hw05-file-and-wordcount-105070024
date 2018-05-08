511e49cd79a2d74460fc392f752e3cd612ccdefc
105070024 <37079519+105070024@users.noreply.github.com>
2018-05-08 23:48:21 +0800

    2.0

--- hw5.py
+++ hw5.py
@@ -1,13 +1,14 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[7]:
 
 
 import string
 import csv
 import json
 import pickle
+from collections import Counter
 
 def main(filename):
     
@@ -20,35 +21,32 @@ def main(filename):
     for line in lines:
         words = line.split()
         for word in words:
-            for ch in string.punctuation:                                                                                                     
-                word=word.strip(ch)
-            all_words.append(word)
+            word = word.strip(string.punctuation)
+            if word:
+                all_words.append(word)
     df = set(all_words)
-    df.remove('')
     for w in df:
         counter = all_words.count(w)
         list1.append(w)
         list1.append(counter)
     
-    with open("wordcount.csv","w+",newline='') as csv_file:
+    with open("wordcount.csv","w",newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['word', 'count'])
         for k in range(0,len(list1),2):
             w=list1[k]
             counter=list1[k+1]
             writer.writerow([w,counter])
-        csv_file.close()
 
     dictionary=dict()
+
     for t in range(0,len(list1),2):
         dictionary[list1[t]]=list1[t+1]
-    with open("wordcount.json","w+",newline='') as json_file:
+    with open("wordcount.json","w") as json_file:
         json.dump(dictionary,json_file)
-        json_file.close()
         
     with open("wordcount.pkl","wb") as pickle_file:
         pickle.dump(dictionary,pickle_file)
-        pickle_file.close()
         
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
